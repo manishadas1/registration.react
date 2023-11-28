92c1314f81f4f510750fb7895748d2048c5c8d31
manishadas1 <manishadas.akrabhi@gmail.com>
2023-11-28 10:23:24 +0530

    registration form complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [successMessage, setSuccessMessage] = useState('');
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    setErrors({
+      ...errors,
+      [name]: '',
+    });
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let valid = true;
+    const newErrors = { ...errors };
+
+    if (formData.username.trim() === '') {
+      newErrors.username = 'Username is required';
+      valid = false;
+    }else if (/\d/.test(formData.username)) {
+      newErrors.username = 'Username should not contain numbers';
+      valid = false;
+    }
+
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(formData.email)) {
+      newErrors.email = 'Invalid email address';
+      valid = false;
+    }
+
+    if (formData.password.length < 6) {
+      newErrors.password = 'Password must be at least 6 characters long';
+      valid = false;
+    }
+
+    if (valid) {
+
+      setTimeout(() => {
+        setFormData({
+          username: '',
+          email: '',
+          password: '',
+        });
+        setSuccessMessage('Registration successful!');
+      }, 1000);
+    } else {
+      setErrors(newErrors);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+          <p className="error">{errors.username}</p>
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+          <p className="error">{errors.email}</p>
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+          <p className="error">{errors.password}</p>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {successMessage && <p className="success">{successMessage}</p>}
     </div>
   );
-}
+};
 
 export default App;
